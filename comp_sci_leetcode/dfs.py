d14b5c1194ddbb396782c44b0751c21b5736df79
Jeffrey <jwan622@gmail.com>
2023-11-28 12:19:05 -0500

    dfs complete

--- comp_sci_leetcode/dfs.py
+++ comp_sci_leetcode/dfs.py
@@ -4,17 +4,21 @@ DFS is a traversal algorithm that goes as deep as possible in one branch before 
 DFS Example Code (Recursive)
 """
 
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    print(start, end=" ")
+def dfs_iterative(graph, start):
+    visited = set()
+    stack = [start]
 
-    for next_node in graph[start] - visited:
-        dfs(graph, next_node, visited)
-    return visited
+    while stack:
+        print('stack: ', stack)
+        vertex = stack.pop()  # this always targets the last added vertex
+        print('popped vertex: ', vertex)
+        if vertex not in visited:  # do not do anything if we visited already
+            print("we visited this vertex =============> ", vertex)
+            visited.add(vertex)
+            # Add neighbors to the stack
+            stack.extend(graph[vertex] - visited)
 
-# xample usage
+# Example graph
 graph = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
          'C': set(['A', 'F']),
@@ -22,4 +26,8 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
 
-dfs(graph, 'A')  # Starting from node 'A'
+# Testing the DFS iterative function
+dfs_iterative(graph, 'A')  # Starting from node 'A'
+
+print('substraction list: ', set([1,2]) - set([1]))
+print('substraction list with strings: ', set(['A','B']) - set(['B']))
