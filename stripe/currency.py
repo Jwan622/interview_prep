388515c476312fb8794a1131a7985a8ffa94c23c
Jeffrey <jwan622@gmail.com>
2024-03-18 12:11:20 -0400

    use dict for stripe

--- stripe/currency.py
+++ stripe/currency.py
@@ -11,32 +11,33 @@ def conversion(from_curr, to_curr):
         left_side, right_side, rate = rate.split(':')
         print('left side: ', left_side)
         if left_side in rate_lookups:
-            rate_lookups[left_side].append([right_side, float(rate)]) # use another dict next time
+            rate_lookups[left_side].append({right_side: float(rate)}) # use another dict next time
         else:
-            rate_lookups[left_side] = [[right_side, float(rate)]]
+            rate_lookups[left_side] = [{right_side: float(rate)}]
 
     print('rate lookups: ', rate_lookups)
     for key, val in rate_lookups.items():
         if key == from_curr:
-            for rate_pair in val:
-                first_rate = rate_pair[0]
-                first_rate_val = rate_pair[1]
-                if first_rate == to_curr:
-                    return rate_pair[1]
-                for subsequent_lookups in rate_lookups.get(first_rate, []):
-                    if subsequent_lookups[0] == to_curr:
-                        print('got in here for subsequent')
-                        print('sbsequent lookup value: ', subsequent_lookups[1])
-                        print('first_rate value: ', first_rate_val)
-                        print('final value: ', subsequent_lookups[1] * first_rate_val)
-                        return round(subsequent_lookups[1] * first_rate_val, 3)
+            for rate_dct in val:
+                for rate_key, rate_val in rate_dct.items():
+                    if rate_key == to_curr:
+                        return rate_val
+                    for subsequent_lookups in rate_lookups.get(rate_key, []):
+                        for rate_key1, rate_val1 in subsequent_lookups.items():
+                            if rate_key1 == to_curr:
+                                print('got in here for subsequent')
+                                print('sbsequent lookup value: ', rate_val1)
+                                print('first_rate value: ', rate_key1)
+                                print('final value: ', rate_val1 * rate_val1)
+                                return round(rate_val1 * rate_val, 3)
 
     for key, val in rate_lookups.items():
         if key == to_curr:
-            for rate_pair in val:
-                if rate_pair[0] == from_curr:
-                    print("1/rate_pair[1]", 1/rate_pair[1])
-                    return round(1/rate_pair[1], 3)
+            for rate_dct in val:
+                for rate_key, rate_val in rate_dct.items():
+                    if rate_key == from_curr:
+                        print("1/rate_pair[1]", 1/rate_val)
+                        return round(1/rate_val, 3)
 
 
 assert conversion("AUD", "USD") == 0.75
