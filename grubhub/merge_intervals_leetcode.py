464e6ccae9085e1740463b74b6b65a4d17425da9
Jeffrey <jwan622@gmail.com>
2024-03-25 02:17:32 -0400

    merge intervals problem

--- grubhub/merge_intervals_leetcode.py
+++ grubhub/merge_intervals_leetcode.py
@@ -1,3 +1,37 @@
 """
 merge intervals @ leetcode
+
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 """
+
+def create_intervals(intervals):
+  intervals.sort(key=lambda x: (x[0], x[1]))
+
+  final_intervals = []
+  possible_lowest, possible_highest = intervals[0]
+  for interval in intervals:
+    low, high = interval
+
+    if low <= possible_highest:
+      possible_highest = high
+
+    if low > possible_highest:
+      final_intervals.append([possible_lowest, possible_highest])
+      possible_lowest = low
+      possible_highest = high
+
+  final_intervals.append([possible_lowest, possible_highest])
+  print('final_intervals: ', final_intervals)
+  return final_intervals
+
+
+intervals1 = [[1,3],[2,6],[8,10],[15,18]]
+assert create_intervals(intervals1) == [[1,6],[8,10],[15,18]]
+
+
+intervals2 = [[1,4],[4,5]]
+assert create_intervals(intervals2) == [[1,5]]
+
+
+intervals3 = [[4,5], [1,4]]
+assert create_intervals(intervals3) == [[1,5]]
--- notion/employee_managers_leetcode.py
+++ notion/employee_managers_leetcode.py
@@ -7,6 +7,19 @@ import pandas as pd
   # print('relevant_employees \n', relevant_employees)
   # return relevant_employees.name
 
+
+"""
+In pandas, using single brackets [] when selecting columns from a DataFrame will return a pandas Series, while double brackets [[]] will return a DataFrame. Here's a quick explanation:
+
+Single Bracket ([]):
+
+When you use single brackets with a single column label, pandas returns a Series representing that column.
+For example: df['column_name'] returns a Series.
+Double Brackets ([[]]):
+
+When you use double brackets, you're passing a list of column labels, and pandas returns a DataFrame that contains just the specified columns.
+Even if the list contains a single column label, you get a DataFrame: df[['column_name']] returns a DataFrame.
+"""
 def find_manager_employees(employee: pd.DataFrame) -> pd.DataFrame:
   # Group by 'managerId' and count the number of employees for each manager
   managers_employee_count = employee.groupby('managerId').agg({'id': 'count'}).rename(columns={'id': 'employee_count'})
@@ -20,7 +33,8 @@ def find_manager_employees(employee: pd.DataFrame) -> pd.DataFrame:
   Column or index level name(s) in the caller to join on the index in other, otherwise joins index-on-index.
   This joins the managers_with_more_than_five DataFrame with the modified employee DataFrame on the managerId column. The on parameter specifies the column in the calling DataFrame (managers_with_more_than_five) that should be aligned with the index of the DataFrame being joined (employee.set_index('id')).
   """
-  relevant_employees = managers_with_more_than_five.join(employee.set_index('id'), on='managerId')
+  relevant_employees = managers_with_more_than_five.merge(employee, left_on='managerId', right_on='id', how='inner')
+  # relevant_employees = managers_with_more_than_five.join(employee.set_index('id'), on='managerId', how='inner')
   print('relevant employees \n', relevant_employees)
 
   # Return the names of the relevant managers
@@ -32,5 +46,13 @@ employee_data = {
     "name": ["John", "Dan", "James", "Amy", "Anne", "Ron"],
     "department": ["A", "A", "A", "A", "A", "B"],
     "managerId": [None, 101, 101, 101, 101, 101]
+}
+print(find_manager_employees(pd.DataFrame(employee_data)))
+
+employee_data = {
+    "id": [101, 102, 103, 104, 105, 106],
+    "name": ["John", "Dan", "James", "Amy", "Anne", "Ron"],
+    "department": ["A", "A", "A", "A", "A", "B"],
+    "managerId": [None, 100, 100, 100, 100, 100] # no matching manager, so should return empty
 }
 print(find_manager_employees(pd.DataFrame(employee_data)))
